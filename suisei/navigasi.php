82f527e0a45e2462cdae2f7ed0a695066e572f9d
RadhitViBritannia <110009337+RadhitViBritannia@users.noreply.github.com>
2022-10-20 11:22:21 +0700

    Add files via upload

---
+++ suisei/navigasi.php
@@ -0,0 +1,13 @@
+<div class="card">
+    <div class="card-body">
+        <h2 class="text-primary text-center">PEDULI DIRI</h2>
+        <p class="text-center text-secondary small">CATATAN PERJALANAN</p>
+</div>
+</div>
+
+<div class="list-group mt-4">
+    <a href="" class="list-group-item">Home</a>
+    <a href="" class="list-group-item">Catatan Perjalanan</a>
+    <a href="" class="list-group-item">Isi Data</a>
+    <a href="" class="list-group-item">Logout</a>
+</div>
